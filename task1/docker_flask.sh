d6e83afa19287724082c2fd7f044ee7bea905edd
Mikhail Rusakovich <rusakovichmv@gmail.com>
2021-11-09 13:14:35 -0500

    5.2 practice version 2.0 5.2 practice version 2.0

--- task1/docker_flask.sh
+++ task1/docker_flask.sh
@@ -2,6 +2,6 @@
 app_image="docker.test"
 container_name="app_container"
 docker build -t ${app_image} .
-docker run -d -p 8080:5000 \
+docker run -d --restart unless-stopped -p 8080:5000 \
   --name=${container_name} \
   -v $PWD:/app ${container_name}
