c0fcf59286c194f5f65cc390c21b65871c34c656
Mikhail Rusakovich <rusakovichmv@gmail.com>
2021-11-08 20:03:16 -0500

    5.2 practice

---
+++ virt-11/5.2/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["net-ssh",[">= 2.6.5","< 7.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["vagrant-vmware-desktop",["= 3.0.1"]]],"checksum":"4bb208d3b71433c62996374cc08b0bd357ad42e49a08b20299d92f52d9e21bf7","vagrant_version":"2.2.18"}
